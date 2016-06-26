580431c597973032c2e9d6effa49638b9da97ad2
guillaume GRAND CLEMENT <guillaume GRAND CLEMENT>
2016-06-26 19:07:44 +0200

    Modify Rx call of SampleRequest

--- app/src/main/java/com/example/guillaume_grand_clement/herowars/ui/activity/impl/MainActivity.java
+++ app/src/main/java/com/example/guillaume_grand_clement/herowars/ui/activity/impl/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.guillaume_grand_clement.herowars.ui.activity.impl;
 
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import com.example.guillaume_grand_clement.herowars.R;
 import com.example.guillaume_grand_clement.herowars.data.pojo.SamplePojo;
 import com.example.guillaume_grand_clement.herowars.network.request.impl.SampleRequest;
 import com.example.guillaume_grand_clement.herowars.ui.activity.AbsActivity;
 
+import rx.Observer;
+import rx.Scheduler;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AbsActivity {
 
@@ -18,15 +18,28 @@ public class MainActivity extends AbsActivity {
     protected void onResume() {
         super.onResume();
         //Tu fais ca pour lancer une requette :
-        mSubscriptions.add(new SampleRequest(this).asObservable().subscribe(new Action1<SamplePojo>() {
+        mSubscriptions.add(new SampleRequest(this).asObservable()
+                //Designe le thread sur lequel la requete sera executé
+                .subscribeOn(Schedulers.io())
+
+                //Designe le thread sur lequel la réponse sera interprété
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(new Observer<SamplePojo>() {
             @Override
-            public void call(SamplePojo samplePojo) {
-                //résultat avec ton pojo en parametre
+            public void onCompleted() {
+                //TODO Lorsque la requete est terminée
+
             }
-        }, new Action1<Throwable>() {
+
             @Override
-            public void call(Throwable throwable) {
-                //En cas d'erreur
+            public void onError(Throwable e) {
+                //TODO Lorsque la requete retourne une erreur
+
+            }
+
+            @Override
+            public void onNext(SamplePojo samplePojo) {
+                //TODO Lorsque la requete retourne quelque chose
+
             }
         }));
     }
