d94d65204f77e93cb110449d59254d02cb49488a
guillaume GRAND CLEMENT <guillaume GRAND CLEMENT>
2016-06-26 18:53:16 +0200

    add Sample request call on MainActivity

--- app/src/main/java/com/example/guillaume_grand_clement/herowars/ui/activity/AbsActivity.java
+++ app/src/main/java/com/example/guillaume_grand_clement/herowars/ui/activity/AbsActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.LayoutRes;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
 import com.example.guillaume_grand_clement.herowars.R;
 
 import butterknife.ButterKnife;
--- app/src/main/java/com/example/guillaume_grand_clement/herowars/ui/activity/impl/MainActivity.java
+++ app/src/main/java/com/example/guillaume_grand_clement/herowars/ui/activity/impl/MainActivity.java
@@ -1,15 +1,38 @@
 package com.example.guillaume_grand_clement.herowars.ui.activity.impl;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.guillaume_grand_clement.herowars.R;
+import com.example.guillaume_grand_clement.herowars.data.pojo.SamplePojo;
+import com.example.guillaume_grand_clement.herowars.network.request.impl.SampleRequest;
+import com.example.guillaume_grand_clement.herowars.ui.activity.AbsActivity;
 
-public class MainActivity extends AppCompatActivity {
+import rx.functions.Action1;
+
+public class MainActivity extends AbsActivity {
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onResume() {
+        super.onResume();
+        //Tu fais ca pour lancer une requette :
+        mSubscriptions.add(new SampleRequest(this).asObservable().subscribe(new Action1<SamplePojo>() {
+            @Override
+            public void call(SamplePojo samplePojo) {
+                //résultat avec ton pojo en parametre
+            }
+        }, new Action1<Throwable>() {
+            @Override
+            public void call(Throwable throwable) {
+                //En cas d'erreur
+            }
+        }));
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_main;
     }
 }
